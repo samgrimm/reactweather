d1ffc060a98c2465438a00ef6e87e6f47e2e2d39
samgrimm <samantha.grimm@gmail.com>
2017-03-07 12:46:45 -0600

    prep repo for heroku

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,10 @@
 var React = require('react');
 
 var WeatherMessage = ( {location, temp} ) =>{
-
   return (
     <div>
     <p>It is {temp} in {location}!</p>
     </div>
   );
-
 }
-
 module.exports = WeatherMessage;
