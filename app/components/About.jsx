472fa4c3943f25724d8c0e20aa0e6980c5f23bb4
samgrimm <samantha.grimm@gmail.com>
2017-03-07 13:50:37 -0600

    added copy and style to the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,15 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This page is what it is all about</p>
+      <h1 className="text-centered">About</h1>
+      <p>This app was built following the <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/"> Udemy Course on React</a>.</p>
+      <p>You can see the code in <a href="https://github.com/samgrimm/reactweather">github</a></p>
+      <p>The following tools were used:</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a></li>
+        <li><a href="http://openweathermap.org">Open Weather Map</a></li>
+        <li><a href="http://foundation.zurb.com/sites/docs/">Foundation</a></li>
+      </ul>
     </div>
   );
 }
